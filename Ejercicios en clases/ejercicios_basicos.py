f1da461ea615daa25a621aa4abfd0265a3feeb01
Jose Figueroa <josearies.9811@gmail.com>
2024-08-24 15:57:34 -0400

    Ejercicios resueltos 29/30

--- Ejercicios en clases/ejercicios_basicos.py
+++ Ejercicios en clases/ejercicios_basicos.py
@@ -56,7 +56,7 @@ else:
     print(f"{num1} No es un número primo")
     
 # 8 "Tabla de Multiplicar: Genera la tabla de multiplicar de un número dado"
-num1 = 5
+num1 = 5 # int(input("Ingresa tu número: ")) #Lo comento para que no moleste xd
 for i in range(1,11):
     resultado = num1 * i
     print(f"{num1} x {i} = {resultado}")
@@ -89,3 +89,152 @@ for i in palabra:
     if i in vocales:
         count_v +=1
 print(f"La palabra tiene {count_v} vocales")
+
+# 12 "Contador de Palabras: Cuenta cuantas palabras tiene una oración dada"
+oracion = "" # (7 palabras)
+count_w = 0
+for i in oracion:
+    if i == " ":
+        count_w +=1
+print(count_w+1)    #Ok, esta es la solución más simple y básica que conozco, bastante mejorable la verdad
+
+# 13 "Invertir una Cadena: Escribe un programa que invierta el orden de los caracteres en una cadena"
+palabra = "Hola Python"
+palabra_reverse = palabra[::-1]
+print(palabra_reverse)
+
+# 14 "Mayor y Menor en una Lista: Encuentra el número mayor y menor en una lista de números"
+lista = [4, 12, 34, 1, 2, 443, -52, 98]
+max_lista = max(lista)
+min_lista = min(lista)
+print(f"El Mayor es {max_lista}\nEl Menor es {min_lista}")
+
+# 15 "Promedio de una Lista: Calcula el promedio de una lista de números"
+lista = [42, 12, 34, 55, 25, 43, 52, 48]
+promedio_lista = sum(lista)/len(lista)
+print(f"El Promedio de la lista es = {promedio_lista}")
+
+# 16 "Elimina los duplicados: Elimina los elementos duplicados de una lista"
+lista_num = [1, 1, 2, 3, 4, 6, 2, 3, 5, 3, 7, 6]
+lista_str = ["Hola", "Como", "Estas", "?", "Como", "Estas", "Usando", "Python", "?"]
+print(lista_num)
+print(lista_str)
+
+lista_num = set(lista_num)
+lista_str = set(lista_str)
+print(lista_num)
+print(lista_str)
+
+# 17 "Múltiplos de 3 y 5: Imprime todos los múltiplos de 3 y 5 entre 1 y 100"
+for i in range(1,101):
+    if i % 3 == 0 and i % 5 == 0:
+        print(f"{i} Multiplo de 3 y 5")
+    # elif i % 3 == 0:
+    #     print(f"{i} Multiplo de 3")
+    # elif i % 5 == 0:
+    #     print(f"{i} Multiplo de 5")  # por si acaso lo escribí tambien xd
+    
+# 18 "Número Fibonacci: Genera la seria de Fibonacci hasta un número n dado"
+limite_f = 39 #int(input("Ingresa un número entero para el limite de la serie de Fibonacci: ")) #comentado para que no moleste
+a, b, c = 0, 1, 0
+while True:
+    c = a + b
+    if c <= limite_f:
+        print(f"{a} + {b} = {c}")
+        a = b
+        b = c
+    else:
+        break
+    
+# 19 "Número Capicúa: Verifica si un número es capicúa (se lee igual al derecho y al revés)"
+while True:
+    try:
+        num1 = 3223 #int(input("Ingresa un número: ")) #comentado para que no moleste
+        num_str = str(num1)
+        num_capicua = num_str[::-1]
+        if num_str == num_capicua:
+            print("El Número es Capicua")
+            break
+        else:
+            print("No es Capicua")
+            break
+    except:
+        print("Ingresa un fokin numero!")
+        
+# 20 "Ordenar Números: Ordena una lista de números de menor a mayor"
+lista = [4, 12, 34, 1, 2, 443, -52, 98]
+lista.sort()
+print(lista)  #Para solo mostrar los números ordenados sin modificar sus indices, podemos usar la función sorted()
+print(sorted(lista))
+
+# 21 "Calculadora Básica: Crea una calculadora simple que permita sumar, restar, multiplicar, y dividir dos números"
+# print("Calculadora Básica")
+# while True:
+#     print("\nOperaciones ('sumar' o '+') ('restar' o '-') ('multiplicar' o '*') ('dividir' o '/')\nSi desea salir del programa ('salir' o 'x')\n")
+#     num1 = float(input("Ingresa el primer número: "))
+#     op = input("Indica la operación: ")
+#     if op == "salir" or op == "x":
+#         break
+#     num2 = float(input("Ingresa el segundo número: "))
+    
+#     if op == "sumar" or op == "+":
+#         print(f"Resultado = {num1 + num2}")
+#     elif op == "restar" or op == "-":
+#         print(f"Resultado = {num1 - num2}")
+#     elif op == "multiplicar" or op == "*":
+#         print(f"Resultado = {num1 * num2}")
+#     elif op == "dividir" or op == "/":
+#         if num2 != 0:
+#             print(f"Resultado = {num1 / num2}")
+#         else:
+#             print("Error! No se puede dividir entre CERO")
+#     else:
+#         print("Error en la operación")
+
+# 22 "Convertir Horas a Segundos: Convierte una cantidad de horas a segundos"
+horas = 2 #float(input("Ingrese la cantidad de horas que quiera convertir a segundos: "))
+segundos_hora = int(horas * 3600)
+print(f"{horas} Horas a Segundos {segundos_hora}")
+
+# 23 "Número de Dígitos: Calcula cuántos dígitos tiene un número"
+num1 = 2
+print(f"el {num1} tiene {len(str(num1))} dígito(s)") #podemos usar condicionales para mostrar el mensaje exacto
+
+# 24 "Suma de Dígitos: Suma todos los dígitos de una número"
+num1 = 123
+num2 = 456
+suma_digit = str(num1) + str(num2)
+print(suma_digit)
+
+# 25 "Área de un Círculo: Calcula el área de un círculo dado su radio."
+radio = 12
+area = 3.14159265 * (radio*radio)
+print(f"El área es = {area}")
+
+# 26 "Conversión de Moneda: Convierte una cantidad de dinero de una moneda a otra (USD a EUR)"
+monto_USD = 22
+valor_EUR = 0.89
+conv_USD_EUR = monto_USD * valor_EUR
+print(f"{monto_USD} USD = {round(conv_USD_EUR,2)} EUR") #La función round(numero) redondea el valor que le indiquemos, si tiene decimales podemos indicar la cantidad que deba mostranos round(numero, cant. decimales a mostrar)
+
+# 27 "Determinar Edad: Calcula la edad de una persona dada su fecha de nacimiento"
+año_actual = 2024
+año_nacimiento = 1998
+print(f"Tienes {año_actual - año_nacimiento} años")
+
+# 28 "Piedra, Papel o Tijera: Implemente el juego de piedra, papel o tijera. (Usuario vs Computadora)"
+opciones = {"piedra", "papel", "tijera"}
+# wins = 0
+# while wins < 3:
+#     op_Usuario = input("Ingresa tu movimiento: ")
+    
+# 29 "Conteo Regresivo: Crea un contador que cuente hacia atrás desde un número n hasta 0"
+num1 = 3
+for i in range(0,num1+1):
+    print(num1-i)
+    
+# 30 "Divisores de un Número: Encuentra todos los divisores de un número dado"
+num1 = 27
+for i in range(1,num1+1):
+    if num1 % i == 0:
+        print(i)
