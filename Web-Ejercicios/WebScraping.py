4c75e849f64d3c8b1e57f2f21c3a7d997f2d1600
Jose Figueroa <josearies.9811@gmail.com>
2024-12-23 23:13:27 -0300

    Aumentando la dificultad del ejercicio, extrauyendo otros datos

--- Web-Ejercicios/WebScraping.py
+++ Web-Ejercicios/WebScraping.py
@@ -18,8 +18,32 @@ soup = BeautifulSoup(html, "html.parser")
 # Extraer titulares (Buscar la etiqueta dentro del código fuente HTML)
 #           soup.find_all(nombreEtiqueta, nombreAtributo_="Etiqueta")
 titulares = soup.find_all("span", class_="titleline")  # Extrae todas las etiquetas y los almacena en un set
+subtextos = soup.find_all("span", class_="subline")
 
+# Crear una lista para almacenar ambos datos extraidos
+titulares_subtextos = []
+
+for titulo, subtext in zip(titulares, subtextos):
+    titulo_text = titulo.text.strip()
+    
+    score_span = subtext.find("span", class_="score")
+    puntos = int(score_span.text.split()[0]) if score_span else 0
+    
+    comentario_span =  subtext.find_all("a")[-1]
+    comentarios = int(comentario_span.text.split()[0]) if "comment" in comentario_span.text else 0
+    
+# Guardamos los datos en diccionarios por cada iteración
+    titulares_subtextos.append({
+        "titulo" : titulo_text,
+        "score" : puntos,
+        "comentario" : comentarios,  
+    })
+
+# filtra el titular más popúlar "con más puntos"
+mas_popular = max(titulares_subtextos, key=lambda x: x["score"])
+
+print(f"\nEste es el más popular {mas_popular}")
 # Mostrar resultados encotrados
 print("\nTitulares encontrados:")
-for titular in titulares:
-    print(f"৹ {titular.text}") # El método .text imprime solo el texto encontrado
+for i, veamos in enumerate(titulares_subtextos, start=1):
+    print(f"{i}. {veamos["titulo"]}\n   {veamos["score"]} puntos - {veamos["comentario"]} comentarios")
